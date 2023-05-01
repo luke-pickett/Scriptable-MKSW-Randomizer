af0c0fabdac2b37ca9de23311c97f314a61cb0fa
engooeer <engooeer@gmail.com>
2023-05-01 00:54:17 -0600

    `

--- MK_SW_Randomizer.js
+++ MK_SW_Randomizer.js
@@ -486,7 +486,8 @@ for (i=0; i < input; i++) {
             break
         }
     }
-    let text = row.addText(`${randomMap["name"]} (${randomMap["cup"]}) ${i}`);
+    // let text = row.addText(`${randomMap["name"]} (${randomMap["cup"]}) ${i}`);
+    let text = "test"
     text.font = Font.boldSystemFont(20);
     text.minimumScaleFactor = 0.5
     text.textColor = Color.white()
