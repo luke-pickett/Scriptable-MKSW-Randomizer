fc087e12cb08ff1fb36bdcfdab818b1e19e68b15
engooeer <engooeer@gmail.com>
2023-05-01 00:35:31 -0600

    `

--- MK_SW_Randomizer.js
+++ MK_SW_Randomizer.js
@@ -474,7 +474,7 @@ function getRandomMap(mapList)
 ///////////////////////////////////
 
 
-for (i=0; i < parseInt(input, 10); i++) {
+for (i=0; i < parseInt(input); i++) {
     let row = widget.addStack();
     row.layoutHorizontally();
 
